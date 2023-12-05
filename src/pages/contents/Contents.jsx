cf2c63b64d3c1520051c90e7b4142c338c13cb6f
spython <emmanuelegbo01@gmail.com>
2023-12-05 15:11:29 +0100

    commit

--- src/pages/contents/Contents.jsx
+++ src/pages/contents/Contents.jsx
@@ -1,13 +1,54 @@
+import { useState } from 'react'
 import './contents.scss'
 
 export default function Contents() {
+  const [selectedTab, setSelectedTab] = useState('all');
+
+  const handleClick = (button) => {
+    setSelectedTab(button);
+  }
+    
+  
   return (
     <div className='contents'>
         <div className="wrapper">
             <div className="top">
-              <span>Confrence</span>
-              <span>/</span>
+              <span >Conferences</span>
+              <p>/</p>
               <span>Tech</span>
+            </div>
+
+            <div className="middle">
+              <p>Tech Conferences</p>
+            </div>
+
+            <div className="bottom">
+              <button 
+                onClick={() => handleClick('all')}
+                className={selectedTab === 'all' ? 'click' : '' }
+              >
+                ALL
+              </button>
+              
+              <button
+                onClick={() => handleClick('frontend')}
+                className={selectedTab === 'frontend' ? 'click' : '' }
+              >
+                FRONTEND
+              </button>
+              
+              <button
+                onClick={() => handleClick('backend')}
+                className={selectedTab === 'backend' ? 'click' : '' }
+              >
+                BACKEND
+              </button>
+              <button
+                onClick={() => handleClick('fullstack')}
+                className={selectedTab === 'fullstack' ? 'click' : '' }
+              >
+                FULLSTACK
+              </button>
             </div>
         </div>
     </div>
