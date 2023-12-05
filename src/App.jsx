e91cfdaabd8171ac52690683e68e632c58fc9ba3
spython <emmanuelegbo01@gmail.com>
2023-12-05 09:46:45 +0100

    modified files

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ function App() {
   return (
     <div className='app'>
       <Topbar/>
+
+      <div className="container">
+        
+      </div>
     </div>
   )
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,22 @@ import './topbar.scss'
 export default function Topbar() {
   return (
     <div className='topbar'>
-        <div className="left">k</div>
+        <div className="left">
+            <ul>
+                <li>Discover</li>
+                <li>Live</li>
+                <li>Jobs</li>
+                <li>Conferences</li>
+            </ul>
+        </div>
+
+        <div className="right">
+            <ul>
+                <li>Log In</li>
+            </ul>
+
+            <button>Get Started</button>
+        </div>
     </div>
   )
 }
