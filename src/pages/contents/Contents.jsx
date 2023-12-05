14531ab68493d23cd5f82520bec2fc0a985cdf09
spython <emmanuelegbo01@gmail.com>
2023-12-05 13:44:00 +0100

    commit

--- src/pages/contents/Contents.jsx
+++ src/pages/contents/Contents.jsx
@@ -4,7 +4,11 @@ export default function Contents() {
   return (
     <div className='contents'>
         <div className="wrapper">
-            <div className="top">goat</div>
+            <div className="top">
+              <span>Confrence</span>
+              <span>/</span>
+              <span>Tech</span>
+            </div>
         </div>
     </div>
   )
