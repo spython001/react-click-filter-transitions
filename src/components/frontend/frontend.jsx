758b2a05ed1c0b40003de11156d14d2bd9ec8935
spython <emmanuelegbo01@gmail.com>
2023-12-05 17:05:52 +0100

    commit

---
+++ src/components/frontend/frontend.jsx
@@ -0,0 +1,28 @@
+import './all.scss'
+
+export default function All() {
+  return (
+    <div className='all'>
+        <div className="wrapper">
+            <span>January 30th - February 1st . Backend</span>
+            <p>THAT Confrence Texas</p>
+            <hr/>
+            
+            <span>Febraury 29th - March 1st . Fullstack</span>
+            <p>DEVWORLD</p>
+            <hr className='line'></hr>
+            
+            <span>March 19th - March 22nd . Backend</span>
+            <p>SQLBits</p>
+            <hr className='line'></hr>
+
+            <span>June 6th - June 7th . Frontend</span>
+            <p>CSS Day</p>
+            <hr className='line'></hr>
+
+            <span>July 11th - July 15th . Frontend</span>
+            <p>Mock Day</p>
+        </div>
+    </div>
+  )
+}
